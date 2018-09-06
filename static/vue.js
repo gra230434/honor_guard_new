85af63ccc236ff2ad83aa74b942041f07f2cd8a3
Chia-Chun Wei <weicc@cs.nctu.edu.tw Activity Groups Contributed projects Personal p>
2018-09-07 05:21:38 +0800

    ver 0.02

--- static/vue.js
+++ static/vue.js
@@ -1,25 +1,15 @@
 Vue.component('menu_button', {
   template: '#child-template',
-  props: ['title'],
+  props: ['title', 'name'],
 });
 
 var wrapper = new Vue({
   el: '#wrapper',
   data: {
     all_session: [
-      '1th 表演影片',
-      '2th 表演影片',
-      '6th 表演影片',
-      '7th 表演影片',
-      '8th 表演影片',
-      '9th 表演影片',
-      '10th 表演影片',
-      '11th 表演影片',
-      '12th 表演影片',
-      '13th 表演影片',
-      '14th 表演影片',
-      '15th 表演影片',
-      '16th 表演影片',
+      {title: 'lists_091_1',  name: '1th 表演影片'},
+      {title: 'lists_092_2',  name: '2th 表演影片'},
+      {title: 'lists_096_6',  name: '6th 表演影片'},
     ],
     lists_091_1: {
       title: '小成',
